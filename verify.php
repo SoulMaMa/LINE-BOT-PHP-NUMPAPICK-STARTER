75738f812a6b979d895b8124c8d12025440f801e
SoulMaMa <45041610+SoulMaMa@users.noreply.github.com>
2018-11-14 23:10:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'XWdRGE9JooNZ+Ht8VQ5z8+AtcRD0hic06+SyyU5NlUYDuxzVtaJoctWtSb6WuIEoBdyKo3tFN8qkKPPWK6HxnbAjUNQUyZaJ0t0Y3R9RydRhQCz1j5rN6GAq478oAefRAeRjAAkHsR27fQbhLZEDogdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
