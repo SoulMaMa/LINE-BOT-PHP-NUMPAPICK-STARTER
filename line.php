b30a8a9c39d1d1d2f629afd47ab2d9f7c74070a9
SoulMaMa <45041610+SoulMaMa@users.noreply.github.com>
2018-11-14 22:25:17 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,8 +1,8 @@
- <?php
+  <?php
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'xnGldo0NBZ0wg43WElEDZPxqxWyOXrkZWzygukXAX1vRiPt8mK5Vx7DIxX1KxJbFBdyKo3tFN8qkKPPWK6HxnbAjUNQUyZaJ0t0Y3R9RydQREJJz0MqWtUSPsLc7agRSLK6gp3g+UfA7EIf6Sc9MygdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -33,3 +33,4 @@ function send_LINE($msg){
 }
 
 ?>
+
