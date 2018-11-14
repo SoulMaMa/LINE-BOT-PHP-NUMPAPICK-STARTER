32fd8a5fac300640fb80e082a4fb5059c18d92c0
SoulMaMa <45041610+SoulMaMa@users.noreply.github.com>
2018-11-15 00:41:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,31 +8,29 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
-	echo "OK";
-	}
-if (!is_null($events['events'])) {
-	echo "line bot";
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-
-			$Topic = "NodeMCU1" ;
-			getMqttfromlineMsg($Topic,$text);
-			   
-			
-		}
-	}
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    $userMessage = strtolower($userMessage);
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "t":
+                    $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
+                case "1":
+                   	 $textReplyMessage = "LED is ON";
+			$replyData = new TextMessageBuilder($textReplyMessage);
+	    }
+		    break;
+        default:
+            $textReplyMessage = json_encode($events);
+            $replyData = new TextMessageBuilder($textReplyMessage);         
+            break;
+    }
 }
 //$Topic = "NodeMCU1" ;
 //$text = "Test";
