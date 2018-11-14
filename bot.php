622cf49e8cf9cc83106f586c7c51d27129274584
SoulMaMa <45041610+SoulMaMa@users.noreply.github.com>
2018-11-15 00:07:26 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -8,20 +8,32 @@ $content = file_get_contents('php://input');
 
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
-	echo "LED IS ON";
-	}
-else if (!is_null($events['ESP'])) {
-	
-	send_LINE($events['ESP']);
-		
-	echo "LED IS OFF";
-	}
+if(!is_null($events)){
+    // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events['events'][0]['replyToken'];
+    $typeMessage = $events['events'][0]['message']['type'];
+    $userMessage = $events['events'][0]['message']['text'];
+    switch ($typeMessage){
+        case 'text':
+            switch ($userMessage) {
+                case "1":
+                    $textReplyMessage = "LED ON";
+                    break;
+                case "0":
+                    $textReplyMessage = "LED OFF";
+                    break;
+                default:
+                    $textReplyMessage = " LED DONT WORKING";
+                    break;                                      
+            }
+            break;
+        default:
+            $textReplyMessage = json_encode($events);
+            break;  
+    }
+}
 if (!is_null($events['events'])) {
-	echo "line bot";
+	//echo "line bot";
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -41,4 +53,9 @@ if (!is_null($events['events'])) {
 	}
 }
 
+// ส่วนของคำสั่งจัดเตียมรูปแบบข้อความสำหรับส่ง
+$textMessageBuilder = new TextMessageBuilder($textReplyMessage);
+ 
+//l ส่วนของคำสั่งตอบกลับข้อความ
+$response = $bot->replyMessage($replyToken,$textMessageBuilder);
 ?>
